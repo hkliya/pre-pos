7162267ca6e2f1d7871eefe9a4dcf54376fbd267
Seaborn Lee <seabornlee.cn@gmail.com>
2017-06-07 11:34:03 +0800

    Complete section 3 practice 4

--- practice/section-3/practice-4.js
+++ practice/section-3/practice-4.js
@@ -1,5 +1,48 @@
 'use strict';
 
-function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+function parse(str) {
+  if (str.length === 1) {
+    return {key: str, count: 1};
+  }
+
+  let arr = str.split("-");
+  return {key: arr[0], count: parseInt(arr[1], 10)};
 }
+
+function expand(collectionA) {
+  let res = [];
+  for (let str of collectionA) {
+    let {key, count} = parse(str);
+    for (let i = 0; i < count; i++) {
+      res.push(key);
+    }
+  }
+  return res;
+}
+function group(expanded) {
+  return expanded.reduce((res, ch) => {
+    let o = res.find((o) => o.key === ch);
+    if (o) {
+      o.count++;
+    } else {
+      res.push({key: ch, count: 1});
+    }
+    return res;
+  }, [])
+}
+function discount(grouped, promotionItems) {
+  return grouped.map((o) => {
+    let count = o.count;
+    if (promotionItems.includes(o.key)) {
+      count -= Math.floor(count / 3);
+    }
+    return {key: o.key, count: count};
+  });
+
+}
+function createUpdatedCollection(collectionA, objectB) {
+  let expanded = expand(collectionA);
+  let grouped = group(expanded);
+  return discount(grouped, objectB.value);
+}
+
